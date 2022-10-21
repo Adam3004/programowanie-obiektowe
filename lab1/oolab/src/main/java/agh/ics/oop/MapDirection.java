fbaf159cf7ac2c9b09848574a79b05e8986dfdbb
Adam <Adam.ja.com@op.pl>
2022-10-21 23:05:54 +0200

    lab 2 done

---
+++ lab1/oolab/src/main/java/agh/ics/oop/MapDirection.java
@@ -0,0 +1,58 @@
+package agh.ics.oop;
+
+
+import java.util.Arrays;
+
+public enum MapDirection {
+    NORTH("Północ", 0, new int[]{0, 1}),
+    EAST("Wschód", 1, new int[]{1, 0}),
+    SOUTH("Południe", 2, new int[]{0, -1}),
+    WEST("Zachód", 3, new int[]{-1, 0});
+
+    private final String message;
+    private final int number;
+    private final int[] unitVector;
+
+    MapDirection(String message, int number, int[] unitVector) {
+        this.message = message;
+        this.number = number;
+        this.unitVector = unitVector;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int[] getUnitVector() {
+        return unitVector;
+    }
+
+    public MapDirection next() {
+        int nextNumber = (getNumber() + 1) % 4;
+        return MapDirection.values()[nextNumber];
+    }
+
+    public MapDirection previous() {
+        int nextNumber = getNumber() - 1;
+        if (nextNumber < 0) {
+            nextNumber = 3;
+        }
+        nextNumber %= 4;
+
+        return MapDirection.values()[nextNumber];
+    }
+
+    public String toUnitVector() {
+        return Arrays.toString(getUnitVector());
+    }
+
+    @Override
+    public String toString() {
+        return getMessage();
+    }
+
+}
